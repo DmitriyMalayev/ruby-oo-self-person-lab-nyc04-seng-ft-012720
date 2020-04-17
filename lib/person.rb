dbf2d8f7672aa1e2e3dd8953ab49bb6ce6a02fd2
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-04-17 19:21:55 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,89 +1,82 @@
-class Person
+require "pry"
+
+class Person 
     attr_accessor :bank_account
-    attr_reader :name
+    attr_reader :name, :happiness, :hygiene 
+
     def initialize(name)
-        @name = name
+        @name = name 
         @bank_account = 25
         @happiness = 8
         @hygiene = 8
-    end
-    def happiness=(persons_happiness)
-        @happiness = persons_happiness
+    end 
+
+    def happiness=(happy) 
+        @happiness = happy 
         if @happiness > 10 
             @happiness = 10 
-        end
-        if @happiness < 0
-            @happiness = 0
-        end
-    end
-    def happiness
-        @happiness
-    end
-    def hygiene=(persons_hygiene)
-        @hygiene = persons_hygiene
-        if @hygiene < 0
-            @hygiene= 0
-        end
-        if @hygiene > 10
-            @hygiene= 10
-        end
-    end
-    def hygiene
-        @hygiene
-    end
-    def clean?
-        if @hygiene > 7
-            return true 
-        else 
-            return false 
-        end
-    end 
-    def happy?
-        if @happiness > 7
-            return true 
-        else 
-            return false 
         end 
+
+        if @happiness < 0
+            @happiness = 0 
+        end 
+    end    
+              
+    def hygiene=(hygiene) 
+        @hygiene = hygiene 
+        if @hygiene > 10 
+            @hygiene = 10 
+        end 
+
+        if @hygiene < 0
+            @hygiene = 0 
+        end 
+    end  
+
+    def clean?
+        @hygiene > 7   
     end
-    def get_paid(chaching)
-        @bank_account += chaching 
-        return "all about the benjamins" 
-    end 
+
+    def happy?
+        @happiness > 7   
+    end
+
+    def get_paid(salary)
+       @bank_account += salary
+       return "all about the benjamins"
+    end
+
     def take_bath
-        self.hygiene=(@hygiene + 4)
-        return "♪ Rub-a-dub just relaxing in the tub ♫" 
-    end 
+        self.hygiene += 4 
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
     def work_out
-        self.hygiene=(@hygiene -3)
-        self.happiness=(@happiness +2)
+        self.hygiene -= 3
+        self.happiness += 2 
         return "♪ another one bites the dust ♫"
-    end 
+    end
+
     def call_friend(friend)
-        self.happiness=(@happiness +3)
-    friend.happiness = friend.happiness + 3
-    if friend.happiness > 10
-        friend.happiness = 10
-        end
-        p "Hi #{friend.name}! It's #{@name}. How are you?"
+        self.happiness += 3 
+        friend.happiness += 3 
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+
     end
-    def start_conversation(friend, topic)
+
+    def start_conversation(person, topic)
         if topic == "politics"
-            self.happiness = (@happiness - 2)
-            friend.happiness = friend.happiness - 2
-            if friend.happiness < 0
-                friend.happiness = 0
-            end
-            return 'blah blah partisan blah lobbyist'
+            self.happiness -= 2  
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist" 
         elsif topic == "weather"
-            self.happiness = (@happiness + 1)
-            friend.happiness = friend.happiness + 1
-            if friend.happiness > 10
-                friend.happiness = 10
-            end
-            return 'blah blah sun blah rain'
-        else
-            return 'blah blah blah blah blah'
-        end
-    
-    end
-end
+            self.happiness += 1  
+            person.happiness +=1 
+            return "blah blah sun blah rain"
+        else 
+            self.happiness 
+            person.happiness 
+            return "blah blah blah blah blah"
+    end 
+end 
+end 
